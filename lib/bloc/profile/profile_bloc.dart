87ba94ecb173ca7d4b667afe7ae2b79ed8c4cc90
B. Islam <b.islam@piteur-studio.dz>
2021-08-18 13:37:50 +0100

    finished vidoe 16

--- lib/bloc/profile/profile_bloc.dart
+++ lib/bloc/profile/profile_bloc.dart
@@ -1,19 +1,115 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:zoodja/repositories/userRepository.dart';
 
 part 'profile_event.dart';
 part 'profile_state.dart';
 
 class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
-  ProfileBloc() : super(ProfileInitial());
+
+  UserRepository _userRepository;
+
+  ProfileBloc({@required UserRepository userRepository }):
+        assert(userRepository!=null),
+        _userRepository=userRepository, super(ProfileState.empty())
+  ;
+
+  ProfileState get initialState => ProfileState.empty();
 
   @override
   Stream<ProfileState> mapEventToState(
     ProfileEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if(event is NameChanged){
+      yield* _mapNameChangedToState(event.name);
+    }else if(event is AgeChanged){
+      yield*  _mapAgeChangedToState(event.age);
+    }
+    else if(event is GenderChanged){
+      yield* _mapGenderChangedToState(event.gender);
+    }
+    else if(event is LocationChanged){
+      yield* _mapLocationChangedToState(event.location);
+    }
+    else if(event is PhotoChanged){
+      yield* _mapPhotoChangedToState(event.photo);
+    }
+    else if(event is InterestedInChanged){
+      yield* _mapInterestedInChangedToState(event.interestedIn);
+    }
+    else if(event is Submitting){
+      final uid=await _userRepository.getUser();
+      yield* _mapSubmittedToState(
+        gender:event.gender,
+        photo:event.photo,
+        name:event.name,
+        userId:uid,
+        age:event.age,
+        location:event.location,
+        interestedIn:event.interestedIn,
+        
+      );
+    }
+
+  }
+
+  Stream <ProfileState>_mapNameChangedToState(String name)async* {
+    yield state.update(
+      isNameEmpty: name==null
+    );
+  }
+
+  Stream <ProfileState> _mapAgeChangedToState(DateTime age) async*{
+    yield state.update(
+        isAgeEmpty: age==null
+    );
+  }
+
+  Stream <ProfileState> _mapGenderChangedToState(String gender)async* {
+    yield state.update(
+        isGenderEmpty: gender==null
+    );
+  }
+
+  Stream <ProfileState> _mapLocationChangedToState(GeoPoint location)async* {
+    yield state.update(
+        isLocationEmpty: location==null
+    );
+  }
+
+  Stream <ProfileState> _mapPhotoChangedToState(File photo) async*{
+    yield state.update(
+        isPhotoEmpty: photo==null
+    );
+  }
+
+  Stream <ProfileState> _mapInterestedInChangedToState(String interestedIn) async*{
+    yield state.update(
+        isInterestedEmpty: interestedIn==null
+    );
+  }
+
+  _mapSubmittedToState(
+      {
+        File photo,
+        String name,
+        String userId,
+        DateTime age,
+        GeoPoint location,
+        String interestedIn,
+        String gender
+      }) async*{
+          yield ProfileState.loading();
+          try{
+            await _userRepository.profileSetup(photo, userId, name, gender, interestedIn, age, location);
+            yield ProfileState.success();
+          }catch(_){
+            yield ProfileState.failure();
+          }
   }
 }
